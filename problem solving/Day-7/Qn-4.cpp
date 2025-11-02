026b8fa648f188aa9605871236a73ccc1e63046c
Godson <songodson209@gmail.com>
2025-11-02 19:48:05 +0530

    Add files via upload

---
+++ problem solving/Day-7/Qn-4.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+	int month;
+	printf("Enter month no(1-12)\n");
+	scanf("%d",&month);
+	switch(month)
+	{
+	case 12:
+	case 1:
+	case 2:
+	printf("Season:Winter \n");
+	break;
+	case 3:
+	case 4:
+	case 5:
+	printf("Season:Spring \n");	
+	break;
+	case 6:
+	case 7:
+	case 8:
+	printf("Season:Summer \n");	
+	break;
+	case 9:
+	case 10:
+	case 11:
+	printf("Season:Autumn \n");	
+	default:
+	printf("invalid enter (1-12)");
+	}
+	return 0;
+}
